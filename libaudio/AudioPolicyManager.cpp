c1279152548056d5e68fceff05527173f75b656d
IMER <admin@096.net.ua>
2014-03-26 10:16:41 +0200

    compiled error, del QCOM FM

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -383,36 +383,7 @@ status_t AudioPolicyManager::setDeviceConnectionState(audio_devices_t device,
 #endif
 
         audio_devices_t newDevice = getNewDevice(mPrimaryOutput, false /*fromCache*/);
-#ifdef QCOM_FM_ENABLED
-        if(device == AUDIO_DEVICE_OUT_FM) {
-            if (state == AudioSystem::DEVICE_STATE_AVAILABLE) {
-                ALOGV("setDeviceConnectionState() changeRefCount Inc");
-<<<<<<< HEAD
-                mOutputs.valueFor(mPrimaryOutput)->changeRefCount(AudioSystem::MUSIC, 1);
-<<<<<<< HEAD
-                newDevice = (audio_devices_t)(getNewDevice(mPrimaryOutput, false) | AUDIO_DEVICE_OUT_FM);
-            } else {
-=======
-                mOutputs.valueFor(mPrimaryOutput)->changeRefCount(AudioSystem::FM, 1);
-                newDevice = (audio_devices_t)(AudioPolicyManagerBase::getNewDevice(mPrimaryOutput, false) | AUDIO_DEVICE_OUT_FM);
-            }
-            else {
->>>>>>> parent of 04a4711... libaudio: update to new fm volume api
-=======
-                newDevice = (audio_devices_t)(AudioPolicyManagerBase::getNewDevice(mPrimaryOutput, false) | AUDIO_DEVICE_OUT_FM);
-            }
-            else {
->>>>>>> parent of 538fa3e... libaudio: fix fm routing to speakerphone
-                ALOGV("setDeviceConnectionState() changeRefCount Dec");
-                mOutputs.valueFor(mPrimaryOutput)->changeRefCount(AudioSystem::FM, -1);
-            }
 
-            AudioParameter param = AudioParameter();
-            param.addInt(String8(AudioParameter::keyHandleFm), (int)newDevice);
-            ALOGV("setDeviceConnectionState() setParameters handle_fm");
-            mpClientInterface->setParameters(mPrimaryOutput, param.toString());
-        }
-#endif
         if(isInCall() && isTunnelOutputEnabled()) {
            //On each keyRouting call, HAL is routing new device to all open outputs
            //Incall mode device switch is taking time than normal
